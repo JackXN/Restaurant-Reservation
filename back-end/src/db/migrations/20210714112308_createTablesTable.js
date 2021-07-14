9675cd74cd97e38364f0708dde79231922e7b22d
Jack Rigan <riganjack40@gmail.com>
2021-07-14 12:04:16 -0600

    added createTablesTable and reservations

--- back-end/src/db/migrations/20210714112308_createTablesTable.js
+++ back-end/src/db/migrations/20210714112308_createTablesTable.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("tables", (table) => {
+      table.increments("table_id").primary();
+      table.string("table_name");
+      table.integer("capacity").unsigned();
+      table.integer("reservation_id").unsigned().defaultTo(null);
+      table
+        .foreign("reservation_id")
+        .references("reservation_id")
+        .inTable("reservations");
+      table.timestamps(true, true);
+    });
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function (knex) {
+    return knex.schema.dropTable("tables");
+  };

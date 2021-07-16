39a18194fd0d54b0aa9a53d6b29035d89381b597
Jack Rigan <riganjack40@gmail.com>
2021-07-16 14:21:56 -0600

    bump

---
+++ front-end/src/components/DisplayTables.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useHistory} from 'react-router-dom';
+import {finishTable} from '../utils/api';
+
+
+function DisplayTables() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DisplayTables
